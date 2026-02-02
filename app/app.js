90079ba88a05853533b8892c88d16d6af2e97ee2
anupsharma329 <anupsharma329@gmail.com>
2026-02-03 00:22:52 +0530

    app changes

--- app/app.js
+++ app/app.js
@@ -32,6 +32,6 @@ app.get('/v1/hello', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
